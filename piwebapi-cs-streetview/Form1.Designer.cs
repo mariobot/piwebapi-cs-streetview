f29908833728313fe71a1ce93b21fa32f9a92f49
Mario Andres Botero Florez <mariobot113@hotmail.com>
2018-12-11 10:34:59 -0500

    Ready project to call maps api

---
+++ piwebapi-cs-streetview/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace piwebapi_cs_streetview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.startTimeLbl = new System.Windows.Forms.Label();
+            this.endTimeLbl = new System.Windows.Forms.Label();
+            this.imageLbl = new System.Windows.Forms.Label();
+            this.timeLbl = new System.Windows.Forms.Label();
+            this.searchTb = new System.Windows.Forms.TextBox();
+            this.SearchBtn = new System.Windows.Forms.Button();
+            this.eventFrameCb = new System.Windows.Forms.ComboBox();
+            this.prevBtn = new System.Windows.Forms.Button();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 450);
+            this.splitter1.TabIndex = 0;
+            this.splitter1.TabStop = false;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer1.Size = new System.Drawing.Size(797, 450);
+            this.splitContainer1.SplitterDistance = 265;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.SearchBtn);
+            this.groupBox1.Controls.Add(this.searchTb);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(6, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(256, 135);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search for new location";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.nextBtn);
+            this.groupBox2.Controls.Add(this.prevBtn);
+            this.groupBox2.Controls.Add(this.eventFrameCb);
+            this.groupBox2.Controls.Add(this.timeLbl);
+            this.groupBox2.Controls.Add(this.imageLbl);
+            this.groupBox2.Controls.Add(this.endTimeLbl);
+            this.groupBox2.Controls.Add(this.startTimeLbl);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(9, 153);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(253, 285);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Get Recorded Sessions";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Location";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Start Time:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "End Time:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Image: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Time";
+            // 
+            // startTimeLbl
+            // 
+            this.startTimeLbl.AutoSize = true;
+            this.startTimeLbl.Location = new System.Drawing.Point(117, 108);
+            this.startTimeLbl.Name = "startTimeLbl";
+            this.startTimeLbl.Size = new System.Drawing.Size(0, 13);
+            this.startTimeLbl.TabIndex = 4;
+            // 
+            // endTimeLbl
+            // 
+            this.endTimeLbl.AutoSize = true;
+            this.endTimeLbl.Location = new System.Drawing.Point(120, 143);
+            this.endTimeLbl.Name = "endTimeLbl";
+            this.endTimeLbl.Size = new System.Drawing.Size(0, 13);
+            this.endTimeLbl.TabIndex = 5;
+            // 
+            // imageLbl
+            // 
+            this.imageLbl.AutoSize = true;
+            this.imageLbl.Location = new System.Drawing.Point(120, 176);
+            this.imageLbl.Name = "imageLbl";
+            this.imageLbl.Size = new System.Drawing.Size(0, 13);
+            this.imageLbl.TabIndex = 6;
+            // 
+            // timeLbl
+            // 
+            this.timeLbl.AutoSize = true;
+            this.timeLbl.Location = new System.Drawing.Point(120, 208);
+            this.timeLbl.Name = "timeLbl";
+            this.timeLbl.Size = new System.Drawing.Size(0, 13);
+            this.timeLbl.TabIndex = 7;
+            // 
+            // searchTb
+            // 
+            this.searchTb.Location = new System.Drawing.Point(84, 20);
+            this.searchTb.Name = "searchTb";
+            this.searchTb.Size = new System.Drawing.Size(100, 20);
+            this.searchTb.TabIndex = 1;
+            // 
+            // SearchBtn
+            // 
+            this.SearchBtn.Location = new System.Drawing.Point(84, 47);
+            this.SearchBtn.Name = "SearchBtn";
+            this.SearchBtn.Size = new System.Drawing.Size(100, 23);
+            this.SearchBtn.TabIndex = 2;
+            this.SearchBtn.Text = "Search";
+            this.SearchBtn.UseVisualStyleBackColor = true;
+            this.SearchBtn.Click += new System.EventHandler(this.SearchBtn_Click);
+            // 
+            // eventFrameCb
+            // 
+            this.eventFrameCb.FormattingEnabled = true;
+            this.eventFrameCb.Location = new System.Drawing.Point(7, 20);
+            this.eventFrameCb.Name = "eventFrameCb";
+            this.eventFrameCb.Size = new System.Drawing.Size(240, 21);
+            this.eventFrameCb.TabIndex = 8;
+            // 
+            // prevBtn
+            // 
+            this.prevBtn.Location = new System.Drawing.Point(7, 59);
+            this.prevBtn.Name = "prevBtn";
+            this.prevBtn.Size = new System.Drawing.Size(110, 23);
+            this.prevBtn.TabIndex = 9;
+            this.prevBtn.Text = "Previous";
+            this.prevBtn.UseVisualStyleBackColor = true;
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Location = new System.Drawing.Point(140, 59);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(107, 23);
+            this.nextBtn.TabIndex = 10;
+            this.nextBtn.Text = "Next";
+            this.nextBtn.UseVisualStyleBackColor = true;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(528, 450);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.splitter1);
+            this.Name = "Form1";
+            this.Text = "PI Web API C# Application";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button nextBtn;
+        private System.Windows.Forms.Button prevBtn;
+        private System.Windows.Forms.ComboBox eventFrameCb;
+        private System.Windows.Forms.Label timeLbl;
+        private System.Windows.Forms.Label imageLbl;
+        private System.Windows.Forms.Label endTimeLbl;
+        private System.Windows.Forms.Label startTimeLbl;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button SearchBtn;
+        private System.Windows.Forms.TextBox searchTb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ piwebapi-cs-streetview/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace piwebapi_cs_streetview
+{
+    public partial class Form1 : Form
+    {
+        static string streetViewBaseUrl = "https://maps.googleapis.com/maps/api/streetview?key=AIzaSyAlCo2dLOV-Dmyt-l4WMCDt7EORd0t8vLc";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchBtn_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(string.Format("{0}&size={1}x{2}", streetViewBaseUrl, webBrowser1.Width - 40, webBrowser1.Height - 40));
+
+            if (searchTb.Text != string.Empty)
+            {
+                sb.Append(string.Format("&location={0}", searchTb.Text));                
+                webBrowser1.Navigate(sb.ToString());
+            }
+            else {
+                MessageBox.Show("Please provide a location to search");
+            }
+
+        
+        }
+    }
+}
